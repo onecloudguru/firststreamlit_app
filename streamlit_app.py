a327c054f0ae16c619cca7a8d5bce49f6a42313f
onecloudguru <142032791+onecloudguru@users.noreply.github.com>
2023-08-11 10:58:42 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,10 @@ import streamlit
 
 streamlit.title('Healthy Diner !!')
 
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣 🥗 🐔  Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥣 🥗 🐔 🥑🍞Kale, Spinach & Rocket Smoothie')
+streamlit.text('🥣 🥗Hard-Boiled Free-Range Egg')
 
-streamlit.header('Breakfast menu')
 
-streamlit.text('Omega 3 and Blueberry oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
