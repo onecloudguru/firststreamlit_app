d871ac53dcc19906272320ff6a7441da817748df
onecloudguru <142032791+onecloudguru@users.noreply.github.com>
2023-08-11 16:40:41 -0500

    Update streamlit_app.py
    added Fruitvice

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 import pandas as pd
+import requests
 
 streamlit.title('Healthy Diner !!')
 
@@ -26,3 +27,8 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 
 streamlit.dataframe(fruits_to_show)
+
+streamlit.header("Fruityvice Fruit Advice!")
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
