211e5535e59fff58bf05fc52c5a8761290d8576f
onecloudguru <142032791+onecloudguru@users.noreply.github.com>
2023-08-11 10:52:48 -0500

    Update streamlit_app.py
    added menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
 streamlit.title('Healthy Diner !!')
+
+
+streamlit.header('Breakfast menu')
+
+streamlit.text('Omega 3 and Blueberry oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
