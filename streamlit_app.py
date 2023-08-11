511321901108b40856d5c03364f886ad10222590
onecloudguru <142032791+onecloudguru@users.noreply.github.com>
2023-08-11 11:19:49 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ streamlit.text('🥣 🥗Hard-Boiled Free-Range Egg')
 
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-my_fruits_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+my_fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
 
 
 # Let's put a pick list here so they can pick the fruit they want to include 
@@ -18,4 +18,4 @@ streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
 # Display the table on the page.
 
-streamlit.dataframe(my_fruits_list)
+streamlit.dataframe(my_fruit_list)
